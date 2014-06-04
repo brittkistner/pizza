07a20a275750403b276980fe372715d3e2e50240
Brittney Kistner <bkistner01@gmail.com>
2014-06-04 15:39:48 -0700

    my initial commit

---
+++ pizza.rb
@@ -0,0 +1,8 @@
+class Pizza
+end
+
+class Topping
+  def initialize(name)
+    @name = name
+  end
+end
---
+++ spec/pizza_spec.rb
@@ -0,0 +1,19 @@
+require './pizza'
+
+describe Pizza do
+  it "exists" do
+    expect(Pizza).to be_a(Class)
+  end
+end
+
+describe Topping do
+  describe '.initialize' do
+    it "sets the name of the topping" do
+      olive = Topping.new('olives')
+      expect(topping.name).to eq('olives')
+    end
+  end
+  it "exists" do
+    expect(Topping).to be_a(Class)
+  end
+end
