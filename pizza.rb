f68cd019fbbd5085009ac641e44397b93a77d00a
Brittney Kistner <bkistner01@gmail.com>
2014-06-04 16:16:52 -0700

    added the toppings initialize method

--- pizza.rb
+++ pizza.rb
@@ -1,10 +1,14 @@
 class Pizza
+  attr_reader :toppings
+  def initialize(topping1)
+    @toppings = topping1
+  end
 end
 
 class Topping
   attr_accessor :name, :vegetarian
-  def initialize(name, vegetarian: false)
-    @name = name
+  def initialize(name1, vegetarian: false)
+    @name = name1
     @vegetarian = vegetarian
   end
 end
